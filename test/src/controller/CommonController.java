3d2c4c02a21232cba631adb86fdacd0422151e1b
mookun <mookun3@naver.com>
2020-05-19 14:04:40 +0900

    USER_NAME_SAMPLE 상수 추가

--- test/src/controller/CommonController.java
+++ test/src/controller/CommonController.java
@@ -10,5 +10,8 @@ public class CommonController {
 	// SERVER_NAME_TAG
 	private static final String SERVER_NAME_TAG = "1";
 	
+	// USER NAME SAMPLE
+	private static final String USER_NAME_SAMPLE = "SYSTEM";
+	
 
 }
