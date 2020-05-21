20596913a2fccadc82009fd3ab527961a0455237
dhkim <dhkim@crewmate.co.kr>
2020-05-21 17:33:14 +0900

    vue for문 예제
    ajax call & v-for 예제

--- thymeleaf/src/main/java/com/example/demo/controller/TestController.java
+++ thymeleaf/src/main/java/com/example/demo/controller/TestController.java
@@ -1,12 +1,14 @@
 package com.example.demo.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
@@ -24,5 +26,22 @@ public class TestController {
 		model.addAttribute("list", list);
 		
 		return "test";
+	}
+	
+	@RequestMapping(value = "/ajaxCallTest")
+	public @ResponseBody HashMap<String, Object> ajaxCallTest() {
+		
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		
+		List<String> list = new ArrayList<>();
+		
+		list.add("A");
+		list.add("B");
+		list.add("C");
+		list.add("D");
+		
+		map.put("list", list);
+		
+		return map;
 	}
 }
